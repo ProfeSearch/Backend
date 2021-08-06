7d50712d0b2c9835961e4399546ae56359b72b1c
YiboWen <yibowen@usc.edu>
2021-08-06 11:51:01 +0800

    Added AreaEnums

--- models/positionModel.js
+++ models/positionModel.js
@@ -1,74 +1,77 @@
 //TODO
-const mongoose = require('mongoose');
-const Faculty = require('./facultyModel');
+const mongoose = require("mongoose");
+const Faculty = require("./facultyModel");
+const AreaEnums = require("../enums/areaEnums");
 
 const positionSchema = new mongoose.Schema({
-    //TODO
-    faculty: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Faculty',
-        required: [true, 'A position must be posted by a faculty!'],
+  //TODO
+  faculty: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Faculty",
+    required: [true, "A position must be posted by a faculty!"],
+  },
+  title: {
+    type: String,
+    required: [true, "A position must have a title!"],
+  },
+  description: {
+    type: String,
+    required: [true, "A position must have a description!"],
+    minlength: [50, "Description should have at least 50 words!"],
+  },
+  positionType: {
+    type: Number,
+    enum: [0, 1, 2],
+    default: 0,
+    required: [true, "A position must have a type!"],
+  },
+  area: {
+    type: String,
+    set: (el) => AreaEnums[el],
+    default: "Unspecified",
+  },
+  location: {
+    type: Number,
+    enum: [0, 1],
+    default: 0,
+  },
+  target: [
+    {
+      type: Number,
+      enum: [0, 1, 2, 3, 4, 5],
     },
-    title: {
-        type: String,
-        required: [true, 'A position must have a title!'],
-    },
-    description: {
-        type: String,
-        required: [true, 'A position must have a description!'],
-        minlength: [50, 'Description should have at least 50 words!'],
-    },
-    positionType: {
-        type: Number,
-        enum: [0, 1, 2],
-        default: 0,
-        required: [true, 'A position must have a type!'],
-    },
-    field: {
-        type: String,
-    },
-    location: {
-        type: Number,
-        enum: [0, 1],
-        default: 0,
-    },
-    target: [
-        {
-            type: Number,
-            enum: [0, 1, 2, 3, 4, 5],
-        },
-    ],
-    deadline: {
-        type: Date,
-        default: new Date(Date.now() + 20 * 24 * 60 * 60 * 1000),
-        required: [true, 'A position must have a deadline!'],
-    },
-    released: {
-        type: Date,
-        default: Date.now(),
-    },
-    status: {
-        type: Number,
-        enum: [0, 1, 2],
-        default: 0,
-        required: [true, 'A position must have a status!'],
-    },
+  ],
+  deadline: {
+    type: Date,
+    default: new Date(Date.now() + 20 * 24 * 60 * 60 * 1000),
+    required: [true, "A position must have a deadline!"],
+  },
+  released: {
+    type: Date,
+    default: Date.now(),
+  },
+  status: {
+    type: Number,
+    enum: [0, 1, 2],
+    default: 0,
+    required: [true, "A position must have a status!"],
+  },
 });
 
-positionSchema.virtual('applications', {
-    ref: 'Application',
-    foreignField: 'position',
-    localField: '_id',
+positionSchema.virtual("applications", {
+  ref: "Application",
+  foreignField: "position",
+  localField: "_id",
 });
 
 positionSchema.pre(/^find/, function (next) {
-    this.populate({
-        path: 'faculty',
-        select: '-__v -user -positions',
-    });
-    next();
+  this.populate({
+    path: "faculty",
+    select: "-__v -user -positions",
+  });
+  next();
 });
 
-const Position = mongoose.model('position', positionSchema);
+const Position = mongoose.model("position", positionSchema);
 
 module.exports = Position;

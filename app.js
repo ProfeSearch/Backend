9ddacc4fecc8e4c2cb1d5d9658b2dff2413d0627
Yuxing Zhou <yuxingz@usc.edu>
2021-08-06 09:38:37 +0800

    made some minor comments

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use('/api', limiter);
 
 //body parser
 app.use(express.json({ limit: '10kb' }));
+// TODO  is 10kb enough? 
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
